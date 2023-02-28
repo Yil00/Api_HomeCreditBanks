b6b7a5dad886a3f1cf94603cb61321ec2570ff0e
Yill00 <mehmetjoob@gmail.com>
2023-02-28 16:42:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -159,51 +159,10 @@ def predict2():
 
     return jsonify( decision_text=predictDecision(ID=ID))
 
-# @app.route("/ClientID", methods = ['GET'])
-# def predict3():
-#     # Liste des identifiants de tous les clients dans le dataset
-#     all_id_client = list(lecture_X_test_original()['sk_id_curr'])
-#     # Récupération de l'ID du client dans l'URL
-#     ID = request.args['id_client']
-#     ID = int(ID)
-# 
-#  
-#     y_pred_lgbm = model_LGBM.predict(lecture_X_test_clean().drop(labels="sk_id_curr", axis=1))    # Prédiction de la classe 0 ou 1
-#     y_pred_lgbm_proba = model_LGBM.predict_proba(lecture_X_test_clean().drop(labels="sk_id_curr", axis=1)) # Prédiction du % de risque
-# 
-#     # Récupération du score du client
-#     y_pred_lgbm_proba_df = pd.DataFrame(y_pred_lgbm_proba, columns=['proba_classe_0', 'proba_classe_1'])
-#     y_pred_lgbm_proba_df = pd.concat([y_pred_lgbm_proba_df['proba_classe_1'],
-#                                     lecture_X_test_clean()['sk_id_curr']], axis=1)
-#     
-#     y_pred_lgbm_df = pd.DataFrame(y_pred_lgbm, columns=['prediction'])
-#     y_pred_lgbm_df = pd.concat([y_pred_lgbm_df, lecture_X_test_clean()['sk_id_curr']], axis=1)
-#     y_pred_lgbm_df['client'] = np.where(y_pred_lgbm_df.prediction == 1, "non solvable", "solvable")
-#     y_pred_lgbm_df['decision'] = np.where(y_pred_lgbm_df.prediction == 1, "refuser", "accorder")
-# 
-# 
-# 
-#     # Vérification que l'ID est présent dans la liste des clients du dataset
-#     if ID not in all_id_client:
-#         # Si l'ID n'est pas valide, renvoie une réponse avec des valeurs "NA"
-#         number="L'identifiant que vous avez saisi n'est pas valide !"
-#         prediction="NA"
-#         solvabilite="NA"
-#         decision="NA"
-#     else :
-#         # Si l'ID est valide, récupère la prédiction de décision pour ce client
-#         number=""
-#         score = y_pred_lgbm_proba_df[y_pred_lgbm_proba_df['sk_id_curr']==ID]
-#         prediction = round(score.proba_classe_1.iloc[0]*100, 1)
-#         solvabilite = y_pred_lgbm_df.loc[y_pred_lgbm_df['sk_id_curr']==ID, "client"].values
-#         solvabilite = solvabilite[0]
-#         decision = y_pred_lgbm_df.loc[y_pred_lgbm_df['sk_id_curr']==ID, "decision"].values
-#         decision = decision[0]
-#     # Retourne une réponse JSON avec la prédiction de décision
-#     return jsonify(identifiant_text=ID)
+##########
 
-host = '0.0.0.0'
-port = 5003
+# host = '0.0.0.0'
+# port = 5003
 #local host : http://127.0.0.1:5003
 #host=host,port=port
 if __name__ == "__main__":
